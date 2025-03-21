bf0ef42adf0052c26a2f54568b1b9ce5976a2f62
fiyon01 <147448591+fiyon01@users.noreply.github.com>
2025-03-21 14:20:52 +0300

    chages

--- peak_app/api/index.js
+++ peak_app/api/index.js
@@ -2,14 +2,6 @@ const serverless = require('serverless-http');
 const dotenv = require("dotenv");
 dotenv.config();
 
-const app = require('../app'); // Import your Express app
+const app = require('../app'); // Import Express app
 
 module.exports = serverless(app); // Serverless handler for Vercel
-
-// Local development server (only runs locally)
-if (process.env.NODE_ENV === 'development') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Local server running at: http://localhost:${PORT}`);
-    });
-}
